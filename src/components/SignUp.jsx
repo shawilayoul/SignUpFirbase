a5c7ba569cf960341cc3216349996a16d6776280
shawil ayoul <aocholayoul9@gmail.com>
2024-04-23 14:22:21 +0200

    adding user email to firebase store

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,41 +1,83 @@
-import React, { useState } from "react";
-import {createUserWithEmailAndPassword} from 'firebase/auth'
-import { auth } from "../firebase";
-import { useNavigate,Link} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { useNavigate, Link } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 const SignUp = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
-    const navigate = useNavigate()
-    const handelSignUp = async(e)=>{
-     e.preventDefault()
-     await createUserWithEmailAndPassword(auth ,email,password).
-      then((userCredential)=>{
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [userSigIn, setUserSignIn] = useState(null);
+  const navigate = useNavigate();
+
+  //geting user info uid
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        setUserSignIn(uid);
+      } else {
+        console.log("logout");
+      }
+    });
+  }, []);
+
+  //console.log(userSigIn);
+
+  const handelSignUp = async (e) => {
+    e.preventDefault();
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
         //Sigined in
         const user = userCredential.user;
-        console.log(user)
+        console.log(user);
+        //adding user email  to firebase store
+        const userReference = addDoc(collection(db, "usernames"), {
+          email: user.email,
+        });
+        console.log(
+          "data was added sucessfully to fire store",
+          userReference.id
+        );
         //navgiate to login page
-        navigate('/signin')
+        navigate("/signin");
       })
-      .catch((error)=>{
-        console.log(error.meassage)
-      })
-    }
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <h2>Sign up</h2>
       <form action="#">
         <div>
           <label htmlFor="email">Email</label>
-          <input type="emain" id="email" onChange={(e)=>setEmail(e.target.value)} />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" onChange={(e)=>setPassword(e.target.value)} />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button type="submit" onClick={handelSignUp}>Sign Up</button>
+        <button type="submit" onClick={handelSignUp}>
+          Sign Up
+        </button>
       </form>
-      <Link to='/signin'><p>Already have an account login </p></Link> 
+      <Link to="/signin">
+        <p>Already have an account login </p>
+      </Link>
     </div>
   );
 };
