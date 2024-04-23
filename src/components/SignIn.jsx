f8dda9e4a40d2ed79582203f7f23d7ce92f57262
shawil ayoul <aocholayoul9@gmail.com>
2024-04-23 10:33:04 +0200

    finshing the signin

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,19 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handelSignIn = async (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth ,email,password)
+    .then((userCredential)=>{
+        const user = userCredential.user;
+        navigate('/')
+        console.log(user)
+    }).catch((error)=>{
+        console.log(error)
+    })
+  };
   return (
     <div>
       <h2>Sign In</h2>
       <form action="#">
         <div>
           <label htmlFor="email">Email</label>
-          <input type="emain" id="email" />
+          <input
+            type="emain"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
-          <label htmlFor="email">Email</label>
-          <input type="emain" id="email" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button>Sign In</button>
+        <button type="submit" onClick={handelSignIn}>Sign In</button>
       </form>
     </div>
   );
